e4ec149d7e7d73f3f5b36f43d03c187051f24d65
dimakarp1996 <dimakarp1996@yandex.ru>
2017-12-05 10:50:22 +0300

    Update PROJECT.py

--- PROJECT.py
+++ PROJECT.py
@@ -1,23 +1,41 @@
 from bs4 import BeautifulSoup
+from lxml import html
 import requests
 def find_files():
     i=0
     url = "https://eda.ru/recepty"
-    urls = []
-    urls.append(url)
+    possible_beginnings=['http://eda.ru/recepty/zavtraki','http://eda.ru/recepty/osnovnye-blyuda',
+                         'http://eda.ru/recepty/sendvichi','http://eda.ru/recepty/sousy-marinady',
+                         'http://eda.ru/recepty/bulony','http://eda.ru/recepty/napitki',
+                         'http://eda.ru/recepty/rizotto','http://eda.ru/recepty/zakuski',
+                         'http://eda.ru/recepty/pasta-picca','http://eda.ru/recepty/supy',
+                         'http://eda.ru/recepty/salaty','http://eda.ru/recepty/vypechka-deserty']
+    
+    ingredients=[]
+    dishes=[]
+    urls = possible_beginnings.copy()
     for url in urls:
-        soup = BeautifulSoup(requests.get(url).text,'lxml')    
-        for a in soup.find_all('a'):
-            if a['href'][:9]=='/recepty/':         
-                address='https://eda.ru'+a['href']
-                if address not in urls:
-                    urls.append(address)
-                    print(i)
-                    print(address)
-                    i+=1
+        for possible_beginning in possible_beginnings:
+            if possible_beginning in url:
+                textt = requests.get(url).text
+                soup = BeautifulSoup(requests.get(url).text,'lxml')    
+                for a in soup.find_all('a'):
+                    address=a['href']
+                    if a['href'][:9]=='/recepty/':         
+                        address='https://eda.ru'+a['href']
+                    if address not in urls:
+                        urls.append(address)
+                        print(i)
+                        print(address)
+                        i+=1
             
     return urls
 
 list_of_links = find_files()
 list_of_receipts = [lambda x: 'recepty' in x for x in list_of_links]
-## show what you've found:
+#это список ссылок на рецепты, проблема - что каждая категория рецептов парсится только до 5й страницы, поэтому рецеплов пока не очень много;
+также не все ссылки ведут собственно на рецепты
+url='https://eda.ru/recepty/salaty/teplij-kartofelnij-salat-s-maslinami-i-percem-21445'
+#пример url , из которого извлекабтся ингредиенты
+data = requests.get(url).text
+#список, из которого извлекаются ингредиенты
