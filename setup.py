76ee11e52aaded38a6842afc97b0628b0cce14a2
dimakarp1996 <dimakarp1996@yandex.ru>
2017-12-05 10:27:52 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+"""Setup script."""
+
+from setuptools import setup
+
+setup(
+    name="CulinaryApp",
+    version="0.0.0",
+    author="Dmitry Karpov, Vyacheslav Trifonov, Andrej Lapushkin",
+    author_email="dimakarp1996@yandex.ru",
+    url="https://github.com/dimakarp1996/CulinaryApp",
+    license="MIT",
+    packages=[
+        "CulinaryApp"
+    ],
+    install_requires=[
+    ],
+    setup_requires=[
+        "pytest-runner",
+        "pytest-pycodestyle",
+        "pytest-cov",
+    ],
+    tests_require=[
+        "pytest",
+        "pycodestyle",
+        "mock"
+    ],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+    ],
+    entry_points={
+        'console_scripts': ['CulinaryApp=CulinaryApp.CulinaryApp:main'],
+    }
+)
