bd56cbc1f6406564aca29fe285399da55d99f459
dimakarp1996 <dimakarp1996@yandex.ru>
2017-12-11 14:59:19 +0300

    Add files via upload

--- test_all.py
+++ test_all.py
@@ -1,7 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 11 14:30:23 2017
+
+@author: tcs-user
+"""
+
 # -*- coding: utf-8 -*-
 """
 Created on Fri Dec  8 18:13:48 2017
-
 @author: DK
 """
 
@@ -14,5 +21,5 @@ import os
 
 def test_find_files():
     link_list = find_files()
-    assert sum([link in beginning for link in link_list for beginning in possible_beginnings])>0
-
+    assert sum(
+        [link in beginning for link in link_list for beginning in possible_beginnings]) > 0
