dcdfd2f0ff8504bd5bb18711cbccf9fcb33d5021
dimakarp1996 <dimakarp1996@yandex.ru>
2017-12-12 11:10:07 +0300

    Add files via upload

--- test_all.py
+++ test_all.py
@@ -1,12 +1,24 @@
-# from CulinaryApp import possible_beginnings
-# from CulinaryApp import find_files
+from CulinaryApp import LinkGetter
+from CulinaryApp import BackEnd
+from CulinaryApp import possible_beginnings
+from CulinaryApp import categories_en
 # import mock
 # import pandas as pd
 # import os
 
 
-# def test_find_files():
-#    link_list = find_files()
-#    assert sum(
-#        [link in beginning for link in link_list
-#         for beginning in possible_beginnings]) > 0
+def test_LinkGetter():
+    obj = LinkGetter(100)
+    obj.get_links()
+    assert sum(
+        [link in beginning for link in obj.urls
+         for beginning in possible_beginnings]) == len(possible_beginnings)
+    tab = obj.get_tab()
+    rus_letters = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+    for i in tab.index:
+        assert tab['category'][i] in categories_en
+        assert tab['name'][i] != 'None'
+        assert len(tab['doses'][i]) > 0
+        assert len(tab['ingredients'][i]) == len(tab['doses'][i])
+        assert sum([x in rus_letters
+                    for x in tab['receipt'][i]]) > len(tab['receipt'][i]) / 2
